75c23ee8e85319f59acaa0d10c3d864a5d9d3577
irakli <irrum6@github.com>
2018-12-12 12:08:14 +0400

    fixed multiple playings

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -2,8 +2,9 @@ let audios = document.getElementsByTagName("audio");
 let len = audios.length;
 let index = () => Math.floor(Math.random() * len);
 for (let i = 0; i < len; i++) {
-    audios[i].addEventListener('ended', () => {
-        audios[index()].play();
-    });
+    audios[i].addEventListener('ended', () => audios[index()].play());
 }
-document.querySelector('button').addEventListener('click', () => audios[index()].play());
+document.querySelector('button').addEventListener('click', (e) => {
+    audios[index()].play();
+    e.target.disabled=true;
+});
