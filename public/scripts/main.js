fe6d33c5adeff06874ba9775b8743f3b073b613b
irakli <irrum6@github.com>
2018-12-26 00:16:19 +0400

    added countdown

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -6,5 +6,31 @@ for (let i = 0; i < len; i++) {
 }
 document.querySelector('button').addEventListener('click', (e) => {
     audios[index()].play();
-    e.target.disabled=true;
+    e.target.disabled = true;
 });
+
+//setup counter and count to next year
+const toDate = new Date(new Date().getUTCFullYear() + 1, 0, 1);
+
+const int = window.setInterval(() => {
+    let diff = toDate - Date.now();
+    if (diff < 0) {
+        window.clearInterval(int);
+        return;
+    } else {
+        //for better accuracy do not use floored results  in consequent calculation
+        let seconds = Math.floor(diff / 1000);
+        let minutes = Math.floor((diff / 1000) / 60);
+        let hours = Math.floor((diff / 1000) / 3600);
+        let days = Math.floor((diff / 1000) / 86400);
+
+        seconds = seconds - (minutes * 60);
+        minutes = minutes - (hours * 60);
+        hours = hours - (days * 24);
+
+        document.getElementById('dd').textContent = days;
+        document.getElementById('hh').textContent = hours;
+        document.getElementById('mm').textContent = minutes;
+        document.getElementById('ss').textContent = seconds;
+    }
+}, 1000);
